b6dc9d7e38bdcb02202f4df5723f9d3defd9997c
Luthfi Izzuddin Hanif <luthfi.izzuddin1@gmail.com>
2023-07-05 23:42:29 +0700

    feat(App.jsx): register dashboard page route

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import SigninPage from './pages/signin';
+import DashboardPage from './pages/dashboard';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/signin" element={<SigninPage />} />
+        <Route path="/" element={<DashboardPage />} />
       </Routes>
     </BrowserRouter>
   );
