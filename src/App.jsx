e667adb81eb37117052ef13a21ccccfb25f937f8
Luthfi Izzuddin Hanif <luthfi.izzuddin1@gmail.com>
2023-07-11 22:19:48 +0700

    feat(categories): register route for each page in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import SigninPage from './pages/signin';
 import DashboardPage from './pages/dashboard';
+import CategoriesPage from './pages/categories';
+import CategoriesCreate from './pages/categories/create';
+import CategoriesEdit from './pages/categories/edit';
 
 function App() {
   return (
@@ -8,6 +11,9 @@ function App() {
       <Routes>
         <Route path="/signin" element={<SigninPage />} />
         <Route path="/" element={<DashboardPage />} />
+        <Route path="/categories" element={<CategoriesPage />} />
+        <Route path="/categories/create" element={<CategoriesCreate />} />
+        <Route path="/categories/edit/:id" element={<CategoriesEdit />} />
       </Routes>
     </BrowserRouter>
   );
