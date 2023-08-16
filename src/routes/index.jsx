7a865adeaf127a920400bb7908c3ec4e0e964cb6
Luthfi Izzuddin Hanif <luthfi.izzuddin1@gmail.com>
2023-08-16 15:00:29 +0700

    refactor(routes): separate routes file and implement protected routes

---
+++ src/routes/index.jsx
@@ -0,0 +1,34 @@
+import { Navigate, Route, Routes } from 'react-router-dom';
+import GuardRoute from '../components/GuardRoute';
+import GuestOnlyRoute from '../components/GuestOnlyRoute';
+
+import Login from '../pages/signin';
+import { HomeRoute } from './HomeRoute';
+import SNavbar from '../components/Navbar';
+
+export function AppRoutes() {
+  return (
+    <Routes>
+      <Route
+        path="login"
+        element={
+          <GuestOnlyRoute>
+            <Login />
+          </GuestOnlyRoute>
+        }
+      />
+      <Route
+        path="/"
+        element={
+          <>
+            <SNavbar />
+            <GuardRoute />
+          </>
+        }
+      >
+        <Route path="dashboard/*" element={<HomeRoute />} />
+        <Route path="" element={<Navigate to="/dashboard" replace={true} />} />
+      </Route>
+    </Routes>
+  );
+}
