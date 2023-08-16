e5c68170988c5d5c4aef963895ca03f6fcb58d3e
Luthfi Izzuddin Hanif <luthfi.izzuddin1@gmail.com>
2023-08-16 16:11:36 +0700

    feat(navbar): implement role-based access on navbar

---
+++ src/components/NavAccess/index.jsx
@@ -0,0 +1,8 @@
+import { Nav } from 'react-bootstrap';
+
+function NavLink({ role, roles, action, children }) {
+  let isHas = roles.indexOf(role);
+  return <>{isHas >= 0 && <Nav.Link onClick={action}>{children}</Nav.Link>}</>;
+}
+
+export default NavLink;
