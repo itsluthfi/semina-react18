bec77e95bb8974d0d6e472c5aab2597cb7d4ffbf
Luthfi Izzuddin Hanif <luthfi.izzuddin1@gmail.com>
2023-08-21 08:53:41 +0700

    feat(redux): create state management for notif

--- src/redux/store.js
+++ src/redux/store.js
@@ -8,12 +8,14 @@ import {
 import thunk from 'redux-thunk';
 import authReducer from './auth/reducer';
 import categoriesReducer from './categories/reducer';
+import notifReducer from './notif/reducer';
 
 const composerEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const rootReducers = combineReducers({
   auth: authReducer,
   categories: categoriesReducer,
+  notif: notifReducer,
 });
 const store = createStore(
   rootReducers,
