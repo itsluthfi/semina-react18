c75b8b9f8d288b6b8a93569125c3e499bb249124
Luthfi Izzuddin Hanif <luthfi.izzuddin1@gmail.com>
2023-08-23 15:18:32 +0700

    feat(redux): create state management for talents

--- src/redux/store.js
+++ src/redux/store.js
@@ -9,6 +9,7 @@ import thunk from 'redux-thunk';
 import authReducer from './auth/reducer';
 import categoriesReducer from './categories/reducer';
 import notifReducer from './notif/reducer';
+import talentsReducer from './talents/reducer';
 
 const composerEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
@@ -16,6 +17,7 @@ const rootReducers = combineReducers({
   auth: authReducer,
   categories: categoriesReducer,
   notif: notifReducer,
+  talents: talentsReducer,
 });
 const store = createStore(
   rootReducers,
