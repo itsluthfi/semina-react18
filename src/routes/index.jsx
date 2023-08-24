19905f38a7d972e494c0af5240da2cc5ec178784
Luthfi Izzuddin Hanif <luthfi.izzuddin1@gmail.com>
2023-08-24 13:16:22 +0700

    feat(payments): implement CRUD for payments page and set up routes for payments

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -6,6 +6,7 @@ import Login from '../pages/signin';
 import { HomeRoute } from './HomeRoute';
 import { CategoriesRoute } from './CategoriesRoute';
 import { TalentsRoute } from './TalentsRoute';
+import { PaymentsRoute } from './PaymentsRoute';
 import SNavbar from '../components/Navbar';
 
 export function AppRoutes() {
@@ -31,6 +32,7 @@ export function AppRoutes() {
         <Route path="dashboard/*" element={<HomeRoute />} />
         <Route path="categories/*" element={<CategoriesRoute />} />
         <Route path="talents/*" element={<TalentsRoute />} />
+        <Route path="payments/*" element={<PaymentsRoute />} />
         <Route path="" element={<Navigate to="/dashboard" replace={true} />} />
       </Route>
     </Routes>
