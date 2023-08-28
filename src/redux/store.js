6e81c5e3d0a0144b22b7c745753ae6fb7a78f21a
Luthfi Izzuddin Hanif <luthfi.izzuddin1@gmail.com>
2023-08-28 15:45:11 +0700

    feat(redux): create state management for orders

--- src/redux/store.js
+++ src/redux/store.js
@@ -13,6 +13,7 @@ import talentsReducer from './talents/reducer';
 import paymentsReducer from './payments/reducer';
 import eventsReducer from './events/reducer';
 import listsReducer from './lists/reducer';
+import ordersReducer from './orders/reducer';
 
 const composerEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
@@ -24,6 +25,7 @@ const rootReducers = combineReducers({
   payments: paymentsReducer,
   events: eventsReducer,
   lists: listsReducer,
+  orders: ordersReducer,
 });
 const store = createStore(
   rootReducers,
