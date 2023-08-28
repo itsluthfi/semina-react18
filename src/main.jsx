f5adf39d452c6010c1cf62e3d091191e3eb5124f
Luthfi Izzuddin Hanif <luthfi.izzuddin1@gmail.com>
2023-08-28 15:46:18 +0700

    style: import react date range css files

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import App from './App.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import 'react-date-range/dist/styles.css';
+import 'react-date-range/dist/theme/default.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
